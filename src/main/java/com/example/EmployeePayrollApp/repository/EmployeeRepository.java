e0d5a45e181c5c86ae39f5a0fd260f396925cf68
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-25 13:26:30 +0530

    [Nivedita]add. just a random comment /hello

--- src/main/java/com/example/EmployeePayrollApp/repository/EmployeeRepository.java
+++ src/main/java/com/example/EmployeePayrollApp/repository/EmployeeRepository.java
@@ -4,6 +4,8 @@ import com.example.EmployeePayrollApp.model.EmployeePayrollData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+// hello
     @Repository
     public interface EmployeeRepository extends JpaRepository<EmployeePayrollData, Integer> {
     }
