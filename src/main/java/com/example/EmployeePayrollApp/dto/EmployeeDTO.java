b49de050766a407d18b7a09bec73559ccdab2dee
Nivedita Vishwakarma <niveditavishwakarma205@gmail.com>
2025-02-27 13:29:58 +0530

    [Nivedita] Add . added the validation in the name field

--- src/main/java/com/example/EmployeePayrollApp/dto/EmployeeDTO.java
+++ src/main/java/com/example/EmployeePayrollApp/dto/EmployeeDTO.java
@@ -1,6 +1,9 @@
 package com.example.EmployeePayrollApp.dto;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -12,6 +15,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class EmployeeDTO {
     private int id;
+    @NotNull(message = "Name cannot be null")
+    @Pattern(regexp = "^[A-Za-z ]{3,50}$", message = "Name must be between 3-50 characters and contain only letters and spaces")
     private String name;
     private double salary;
 
